af7dca3814af2654fb7f737a18fdb50b11b9ede4
Lukas Tröbinger <lukas.troebinger@gmail.com>
2023-06-15 11:03:56 +0200

    bugfixing

--- dns_resolver.py
+++ dns_resolver.py
@@ -10,12 +10,12 @@ def resolve_dns():
 
     try:
         # Make the DNS query for the A record of the domain
-        response = resolver.query(domain_name, 'A')
+        response = resolver.resolve(domain_name, 'A')
         ip_addresses = [str(rdata) for rdata in response]
         print(f"The IP addresses of {domain_name} are: {', '.join(ip_addresses)}")
     except dns.resolver.NXDOMAIN:
         print(f"The domain {domain_name} does not exist.")
     except dns.exception.DNSException as e:
-        print(f"An error occurred: {str(e)}")
+        print(f"An error occurred: {e}")
 
 resolve_dns()
