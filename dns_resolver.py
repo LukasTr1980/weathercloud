3dd3ab401d863ae68bdde5a394c22905cf1594d2
Lukas Tröbinger <lukas.troebinger@gmail.com>
2023-06-15 11:02:52 +0200

    dns resolver to avoid local dns queries

---
+++ dns_resolver.py
@@ -0,0 +1,21 @@
+import dns.resolver
+
+def resolve_dns():
+    # Define the domain name to resolve
+    domain_name = 'api.weathercloud.net'
+
+    # Create a resolver object
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = ['8.8.8.8']
+
+    try:
+        # Make the DNS query for the A record of the domain
+        response = resolver.query(domain_name, 'A')
+        ip_addresses = [str(rdata) for rdata in response]
+        print(f"The IP addresses of {domain_name} are: {', '.join(ip_addresses)}")
+    except dns.resolver.NXDOMAIN:
+        print(f"The domain {domain_name} does not exist.")
+    except dns.exception.DNSException as e:
+        print(f"An error occurred: {str(e)}")
+
+resolve_dns()
