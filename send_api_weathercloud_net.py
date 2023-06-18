bc6c215cd0296f13ef9258ff8bdcf8ccf5a966d0
LukasTr1980 <58291847+LukasTr1980@users.noreply.github.com>
2023-06-18 10:32:37 +0200

    bugfixing

--- send_api_weathercloud_net.py
+++ send_api_weathercloud_net.py
@@ -13,7 +13,6 @@ def resolve_hostname(hostname):
 
 def send_weathercloud(ip_address, data):
     endpoint = '/v01/set'
-
     url = f'http://{ip_address}{endpoint}'
 
     headers = {
@@ -23,17 +22,53 @@ def send_weathercloud(ip_address, data):
     }
 
     try:
-        # Extract the query string parameters from the received data
-        params = {}
-        query_string = data.decode().split(' ')[1]  # Get the query string from the HTTP GET request
-        query_params = query_string.split('&')  # Split the query string into individual parameters
+        params = {
+            'wid': '',
+            'key': '',
+            'date': '',
+            'time': '',
+            'tempin': '',
+            'humin': '',
+            'temp': '',
+            'hum': '',
+            'temp1': '',
+            'hum1': '',
+            'dewin': '',
+            'dew': '',
+            'dew1': '',
+            'chill': '',
+            'chill1': '',
+            'heatin': '',
+            'heat': '',
+            'heat1': '',
+            'thw': '',
+            'thw1': '',
+            'bar': '',
+            'wspd': '',
+            'wspdhi': '',
+            'wdir': '',
+            'wspdavg': '',
+            'wdiravg': '',
+            'rainrate': '',
+            'rain': '',
+            'solarrad': '',
+            'uvi': '',
+            'battery': '',
+            'battery1': '',
+            'ver': '',
+            'type': ''
+        }
+
+        query_string = data.decode().split(' ')[1]
+        query_params = query_string.split('&')
         for param in query_params:
             if '=' in param:
                 key, value = param.split('=')
-                params[key] = value
+                if key in params:
+                    params[key] = value
 
         response = requests.get(url, params=params, headers=headers)
-        response.raise_for_status()  # Raise an exception if the status code indicates an error
+        response.raise_for_status()
         print('Data sent successfully to Weathercloud.')
     except requests.exceptions.RequestException as e:
         print('Error sending data to Weathercloud:', e)
