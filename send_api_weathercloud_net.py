8f5e9a9f833dc1a8b0bf32f40de9713c70b71914
LukasTr1980 <58291847+LukasTr1980@users.noreply.github.com>
2023-06-18 10:13:40 +0200

    bugfixing

--- send_api_weathercloud_net.py
+++ send_api_weathercloud_net.py
@@ -23,7 +23,15 @@ def send_weathercloud(ip_address, data):
     }
 
     try:
-        response = requests.get(url, params=data, headers=headers)
+        # Extract the query string parameters from the received data
+        params = {}
+        query_string = data.decode().split(' ')[1]  # Get the query string from the HTTP GET request
+        query_params = query_string.split('&')  # Split the query string into individual parameters
+        for param in query_params:
+            key, value = param.split('=')
+            params[key] = value
+
+        response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()  # Raise an exception if the status code indicates an error
         print('Data sent successfully to Weathercloud.')
     except requests.exceptions.RequestException as e:
